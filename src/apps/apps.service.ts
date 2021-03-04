7d7b7361b796e9d0316a49240d386ea3aa355d41
Adrian <adrian.rom64@gmail.com>
2021-03-04 10:52:14 +0100

    stuff

--- src/apps/apps.service.ts
+++ src/apps/apps.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { BlockchainReadService } from 'src/blockchain/blockchain.read.service'
 import { BlockchainWriteService } from '../blockchain/blockchain.write.service'
 import config from '../config'
 
 @Injectable()
 export class AppsService {
   constructor(
-    private readonly blockchainWriteService: BlockchainWriteService
+    private readonly blockchainWriteService: BlockchainWriteService,
+    private readonly blockchainReadService: BlockchainReadService
   ) {}
 
   async rbbOpen(device: string) {
-    // TODO
-    // get key from data service
+    const regex = 'key_.{32,44}'
+    const keys = await this.blockchainReadService.fetchWithRegex(regex)
+    console.log(keys)
 
     // const txHash = await this.blockchainWriteService.interactWithDevice(
     //   'open',
