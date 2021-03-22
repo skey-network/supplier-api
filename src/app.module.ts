f90f644c4db1a6e7c8e51d4e81138b4fdc16ddbd
Adrian <adrian.rom64@gmail.com>
2021-03-22 12:30:57 +0100

    fix logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,6 +41,6 @@ import { SupplierModule } from './supplier/supplier.module'
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*')
+    consumer.apply(LoggerMiddleware).forRoutes('/')
   }
 }
